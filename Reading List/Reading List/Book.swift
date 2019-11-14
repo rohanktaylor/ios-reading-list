2b76d5badabc0e335e9ea31a54bb67aa884820ae
Rohan Taylor <rhntaylor@gmail.com>
2019-11-13 18:42:47 -0800

    hadTroubleDevelopingFunctions

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,9 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Rohan Taylor on 11/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,9 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Rohan Taylor on 11/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
